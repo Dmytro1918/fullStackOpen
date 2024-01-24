78b276f26d56ae4465c07b6fc11ed4722006089f
Kameniev Di <kamenevdv1997@gmail.com>
2024-01-24 23:22:07 +0200

    first tasj

---
+++ Tasks_fullstack_course.js
@@ -0,0 +1,17 @@
+// sequenceDiagramForPosting:
+   
+    // participant browser
+    // participant server
+
+    // browser->>server: POST method https://studies.cs.helsinki.fi/exampleapp/new_note_spa
+    // activate server
+    // server-->>browser:sending response JSON object with 
+    // status: 200 OK
+    // deactivate server
+
+    // browser->>server: POST method https://studies.cs.helsinki.fi/exampleapp/new_note_spa
+    // activate server
+    // server -> browser : sending new HTML file 
+    // browser: renders the new HTML file 
+
+  
